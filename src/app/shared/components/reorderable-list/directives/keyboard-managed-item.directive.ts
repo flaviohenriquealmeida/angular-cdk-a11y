9b96d63b769426a35c0dfcd077a52537c91e5457
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-06-18 14:35:21 +0100

    video5.2

--- src/app/shared/components/reorderable-list/directives/keyboard-managed-item.directive.ts
+++ src/app/shared/components/reorderable-list/directives/keyboard-managed-item.directive.ts
@@ -1,6 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appKeyboardManagerItem]'
 })
-export class KeyboardManagedItemDirective {}
+export class KeyboardManagedItemDirective implements OnInit {
+
+  private keyHandlers: Map<string, (event: KeyboardEvent) => void> = new Map();
+
+  public ngOnInit(): void {
+    this.initializeKeyHandlers();
+  }
+
+  private initializeKeyHandlers(): void {
+    this.keyHandlers.set('Tab', event => {
+      console.log('Tab');
+    });
+
+    this.keyHandlers.set('ArrowUp', event => {
+      console.log('ArrowUp');
+    });
+
+    this.keyHandlers.set('ArrowDown', event => {
+      console.log('ArrowDown');
+    });
+
+    this.keyHandlers.set(' ', event => {
+      console.log('Space');
+    });
+
+    this.keyHandlers.set('Escape', event => {
+      console.log('Escape');
+    });
+  }
+}
