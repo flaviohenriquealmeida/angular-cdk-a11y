45b62ec236be703b420438522d79f5213e0729f1
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-06-18 14:44:16 +0100

    video5.3

--- src/app/shared/components/reorderable-list/directives/keyboard-managed-item.directive.ts
+++ src/app/shared/components/reorderable-list/directives/keyboard-managed-item.directive.ts
@@ -1,35 +1,7 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive } from '@angular/core';
 
 @Directive({
   selector: '[appKeyboardManagerItem]'
 })
-export class KeyboardManagedItemDirective implements OnInit {
-
-  private keyHandlers: Map<string, (event: KeyboardEvent) => void> = new Map();
-
-  public ngOnInit(): void {
-    this.initializeKeyHandlers();
-  }
-
-  private initializeKeyHandlers(): void {
-    this.keyHandlers.set('Tab', event => {
-      console.log('Tab');
-    });
-
-    this.keyHandlers.set('ArrowUp', event => {
-      console.log('ArrowUp');
-    });
-
-    this.keyHandlers.set('ArrowDown', event => {
-      console.log('ArrowDown');
-    });
-
-    this.keyHandlers.set(' ', event => {
-      console.log('Space');
-    });
-
-    this.keyHandlers.set('Escape', event => {
-      console.log('Escape');
-    });
-  }
+export class KeyboardManagedItemDirective {
 }
--- src/app/shared/components/reorderable-list/directives/keyboard-manager.directive.ts
+++ src/app/shared/components/reorderable-list/directives/keyboard-manager.directive.ts
@@ -1,6 +1,43 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appKeyboardManager]'
 })
-export class KeyboardManagerDirective {}
+export class KeyboardManagerDirective implements OnInit {
+
+  private keyHandlers: Map<string, (event: KeyboardEvent) => void> = new Map();
+
+  public ngOnInit(): void {
+    this.initializeKeyHandlers();
+  }
+
+  private initializeKeyHandlers(): void {
+    this.keyHandlers.set('Tab', event => {
+      console.log('Tab');
+    });
+
+    this.keyHandlers.set('ArrowUp', event => {
+      console.log('ArrowUp');
+    });
+
+    this.keyHandlers.set('ArrowDown', event => {
+      console.log('ArrowDown');
+    });
+
+    this.keyHandlers.set(' ', event => {
+      console.log('Space');
+    });
+
+    this.keyHandlers.set('Escape', event => {
+      console.log('Escape');
+    });
+  }
+
+  @HostListener('keydown', ['$event'])
+  public manageKey(event: KeyboardEvent): void {
+      const handler = this.keyHandlers.get(event.key);
+      if (handler) {
+        handler(event);
+      }
+  }
+}
